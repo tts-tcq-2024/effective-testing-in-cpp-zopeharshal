7456a15408b8218c71bb0e08abe137b513c9e3bc
zopeharshal <harshalzope99@gmail.com>
2024-08-13 10:10:36 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -3,22 +3,21 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
-        sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
-        sizeName = 'M';
-    } else if(cms > 42) {
-        sizeName = 'L';
+    if (cms < 38) {
+        sizeName = 'S'; // Small
+    } else if (cms > 38 && cms < 42) {
+        sizeName = 'M'; // Medium
+    } else if (cms > 42) {
+        sizeName = 'L'; // Large
     }
     return sizeName;
 }
 
 int main() {
-    assert(size(37) == 'S');   
-    assert(size(38) == 'M');   
-    assert(size(40) == 'M');   
-    assert(size(42) == 'L');   
-    assert(size(43) == 'L');   
-    std::cout << "All is well (maybe!)\n";
+    assert(size(37) == 'S');
+    assert(size(38) == 'M');
+    assert(size(40) == 'M');
+    assert(size(43) == 'L');
+    printf("All is well (maybe!)\n");
     return 0;
 }
