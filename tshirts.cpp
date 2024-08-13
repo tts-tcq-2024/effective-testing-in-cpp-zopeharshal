87fc65fa33196a4deeb5a4b7b8b4727b8bed3626
zopeharshal <harshalzope99@gmail.com>
2024-08-13 10:07:09 +0530

    Update tshirts.cpp

--- tshirts.cpp
+++ tshirts.cpp
@@ -14,9 +14,11 @@ char size(int cms) {
 }
 
 int main() {
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
+    assert(size(37) == 'S');   
+    assert(size(38) == 'M');   
+    assert(size(40) == 'M');   
+    assert(size(42) == 'L');   
+    assert(size(43) == 'L');   
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
